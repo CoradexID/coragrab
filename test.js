6382dcd1ce774efa1fb2984ccd93cae1e4480e5b
coradex <coradex06@gmail.com>
2022-06-19 20:55:54 +0700

    Scripted auto-commit on change (2022-06-19 20:55:53) by gitwatch.sh

--- test.js
+++ test.js
@@ -9,17 +9,15 @@ const Database = require('./App/Database/' + process.env.MAIN_THEME + '.js');
 const db = new Database(storage);
 const scraper = require('./App/Scraper/' + process.env.MAIN_TARGET + '.js');
 
-const url = 'https://kiryuu.id/the-newbie-is-too-strong-chapter-3/';
-const chapter = '3';
+const url = 'https://kiryuu.id/manga/how-to-kill-a-god/';
 
 (async () => {
   storage.connect();
   db.connection.connect();
   
-  const data = await scraper.getChapter(url);
-  data.chapter = chapter;
+  const data = await scraper.getManga(url);
   
-  const result = await db.insertChapter(6, data);
+  const result = await db.insertManga(data);
   console.log(result);
   
   console.log('udahan');
