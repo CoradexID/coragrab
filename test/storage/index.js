ad688f2e6e6369899e46001a33f5dc7f6174e9a4
coradex <coradex06@gmail.com>
2022-06-23 10:12:28 +0700

    Scripted auto-commit on change (2022-06-23 10:12:28) by gitwatch.sh

--- test/storage/index.js
+++ test/storage/index.js
@@ -7,7 +7,8 @@ const storage = require(process.env.HOME_DIR + 'App/Storage.js');
   await storage.connectFTP();
   
   try {
-    await storage.uploadToWP('/sdcard/image.png', 'oke/woy');
+    await storage.uploadToWP('/sdcard/image.png', 'oke/woy/test1.png');
+    await storage.uploadToWP('/sdcard/image.png', 'oke/woy/test2.png');
   } catch (e) {}
   
   storage.closeFTP();
