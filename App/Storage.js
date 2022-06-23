89e32add630fd511e390b2d77e174cc541cdf33e
coradex <coradex06@gmail.com>
2022-06-23 10:05:07 +0700

    Scripted auto-commit on change (2022-06-23 10:05:07) by gitwatch.sh

--- App/Storage.js
+++ App/Storage.js
@@ -1,98 +1,57 @@
-const fs = require('fs');
-const Ftp = require('ftp');
-
-const functions = require('./Functions.js');
+const ftp = require('ftp-basic');
 require('dotenv').config();
 
+const functions = require('./Functions.js');
 
 class Storage {
 
   constructor() {
-    this.wp = new Ftp();
-    this.storage = new Ftp();
+    this.wp = new ftp.Client();
+    this.storage = new ftp.Client();
   }
-  
+
   async connectFTP() {
-    this.wp.connect({
+    await this.wp.access({
       host: process.env.WP_FTP_HOST,
       port: process.env.WP_FTP_PORT,
       user: process.env.WP_FTP_USER,
       password: process.env.WP_FTP_PASS,
-      keepalive: 5000
     });
-    this.storage.connect({
+    await this.storage.connect({
       host: process.env.STORAGE_FTP_HOST,
       port: process.env.STORAGE_FTP_PORT,
       user: process.env.STORAGE_FTP_USER,
       password: process.env.STORAGE_FTP_PASS,
-      keepalive: 5000
     });
-    await this.checkConnection(this.wp);
-    await this.checkConnection(this.storage);
     return Promise.resolve(true);
-  }
-  
-  closeFTP() {
-    this.storage.end();
-    this.wp.end();
-  }
-  
-  checkConnection(ftp) {
-    return new Promise((resolve) => {
-      ftp.on('ready', resolve(true));
-    });
   }
 
   async uploadToWP(imagePath, path) {
     const wp = this.wp;
-    console.log('Uploadtowp');
+
     let folders = path.split('/');
     folders.pop();
     folders = folders.join('/');
-    
-    wp.status((err, status) => {
-      if (err) throw err;
-      console.log(status);
-    });
-    
-    console.log('A');
-    await new Promise((resolve, reject) => {
-      wp.mkdir(folders, true, function (err) {
-        if (err) console.log(err);
-    console.log('B');
-        wp.put(imagePath, path, function(err) {
-          if (err) console.log(err);
-    console.log('C');
-          resolve(true);
-        })
-      });
-    });
-    
+
+    await wp.ensureDir(folders);
+    await wp.cd('/');
+
+    await wp.uploadFrom(imagePath, path);
+
     return Promise.resolve(true);
   }
-  
+
   async uploadsToStorage(paths, destination) {
     const storage = this.storage;
     const promises = [];
+
+    for (const path of paths) {
+      const filename = path.split('/').pop();
+      const filepath = destination + filename;
+      
+      await uploadFrom(path, filepath);
+    }
     
-    await new Promise((resolve) => {
-      storage.mkdir(destination, true, () => {
-        for (const path of paths) {
-          const filename = path.split('/').pop();
-          const filepath = destination + filename;
-          const func = new Promise((resolve) => {
-            storage.put(path, filepath, (e) => {
-              if (e) console.log(e);
-              resolve();
-            });
-          });
-          promises.push(func);
-        }
-        resolve(true);
-      });
-    })
-    
-    await Promise.all(promises);
     return Promise.resolve(true);
   }
 
