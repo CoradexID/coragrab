b72e7e795ba5002ad8f942f84690e0ae3251e153
coradex <coradex06@gmail.com>
2022-06-25 23:43:37 +0700

    Scripted auto-commit on change (2022-06-25 23:43:37) by gitwatch.sh

---
+++ test/mangareader/postBoth.test.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const functions = require(process.env.HOME_DIR + 'App/Functions.js');
+const storage = require(process.env.HOME_DIR + 'App/Storage.js');
+const Database = require(process.env.HOME_DIR + 'App/Database/' + process.env.MAIN_THEME + '.js');
+const scraper = require(process.env.HOME_DIR + 'App/Scraper/' + process.env.MAIN_TARGET + '.js');
+
+(async () => {
+  await storage.connectFTP();
+
+  const db = new Database(storage);
+  await db.connectDatabase();
+
+  const url = 'https://kiryuu.id/manga/super-smartphone/';
+
+  const data = await scraper.getManga(url);
+  data.title = 'True Friend Stab You In The Front';
+  console.log(data);
+  const result = await db.insertManga(data);
+  console.log(result);
+
+  const chapterData = await scraper.getChapter(url);
+  chapterData.chapter = '1';
+  chapterData.title = 'True Friend Stab You In The Front Chapter 1';
+  console.log(chapterData);
+  const chapterResult = await db.insertChapter(result.ID, data);
+  console.log(chapterResult);
+
+  storage.closeFTP();
+  db.closeDatabase();
+})();
