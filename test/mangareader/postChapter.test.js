5625129074a6a5e26777d2c526a6b9f81d49efdd
coradex <coradex06@gmail.com>
2022-06-25 17:36:42 +0700

    Scripted auto-commit on change (2022-06-25 17:36:41) by gitwatch.sh

---
+++ test/mangareader/postChapter.test.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const functions = require(process.env.HOME_DIR + 'App/Functions.js');
+const storage = require(process.env.HOME_DIR + 'App/Storage.js');
+const Database = require(process.env.HOME_DIR + 'App/Database/' + process.env.MAIN_THEME + '.js');
+const scraper = require(process.env.HOME_DIR + 'App/Scraper/' + process.env.MAIN_TARGET + '.js');
+
+(async () => {
+  await storage.connectFTP();
+
+  const db = new Database(storage);
+  await db.connectDatabase();
+  
+  const url = 'https://kiryuu.id/super-smartphone-chapter-3/';
+  
+  const data = await scraper.getChapter(url);
+  data.chapter = '1';
+  console.log(data);
+  const result = await db.insertChapter(data);
+  console.log(result);
+  
+  storage.closeFTP();
+  db.closeDatabase();
+})();
