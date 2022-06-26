b55624a67bd2ae8810cd042b1bd68155921f99d7
coradex <coradex06@gmail.com>
2022-06-26 17:13:08 +0700

    Scripted auto-commit on change (2022-06-26 17:13:06) by gitwatch.sh

---
+++ test/database/getChapters.test.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const functions = require(process.env.HOME_DIR + 'App/Functions.js');
+const storage = require(process.env.HOME_DIR + 'App/Storage.js');
+const Database = require(process.env.HOME_DIR + 'App/Database/' + process.env.MAIN_THEME + '.js');
+const scraper = require(process.env.HOME_DIR + 'App/Scraper/' + process.env.MAIN_TARGET + '.js');
+
+
+async function run(db) {
+  const db = new Database();
+  await db.connectDatabase();
+  
+  const chapters = await db.getChapters('586641');
+  
+  db.closeDatabase();
+  
+  return Promise.resolve(true);
+}
+
+(async () => {
+  await run(db);
+})();
