dcc2963020e094865122eeb1e0e0daf4e2077c78
coradex <coradex06@gmail.com>
2022-06-28 19:58:52 +0700

    Scripted auto-commit on change (2022-06-28 19:58:52) by gitwatch.sh

--- test/storage/test.js
+++ test/storage/test.js
@@ -1,16 +1,7 @@
 var ftp = require("basic-ftp");
 
 (async() => {
-  const wp = new ftp.Client(0);
+  const wp = new ftp.Client();
   
-  wp.access({
-    host: "185.193.66.3",
-    user: "corawp",
-    password: "corawp",
-    port: "21"
-  });
-  
-  await wp.cd('/');
-  
-  wp.close();
+  console.log(wp.closed);
 })();
