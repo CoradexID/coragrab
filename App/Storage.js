b6aae01d94352665617208a25e01adc60076d3d3
coradex <coradex06@gmail.com>
2022-06-28 19:54:43 +0700

    Scripted auto-commit on change (2022-06-28 19:54:43) by gitwatch.sh

--- App/Storage.js
+++ App/Storage.js
@@ -5,28 +5,22 @@ const functions = require('./Functions.js');
 
 class Storage {
 
-  constructor(options) {
+  constructor(config) {
     this.client = new ftp.Client();
-    this.options = options;
-    
-    this.host = this.options.host;
-    this.user = this.options.user;
-    this.password = this.options.password;
-    this.port = this.options.port;
+    this.config = config;
   }
 
   async connectFTP() {
-    await this.storage.access({
-      host: process.env.STORAGE_FTP_HOST,
-      port: process.env.STORAGE_FTP_PORT,
-      user: process.env.STORAGE_FTP_USER,
-      password: process.env.STORAGE_FTP_PASS,
+    await this.client.access({
+      host: this.config.host,
+      user: this.config.user,
+      password: this.config.password,
+      port: this.config.port,
     });
     
-    this.wp.ftp.verbose = true;
     return Promise.resolve(true);
     
-    // await this.wp.access({
+    // await this.client.access({
     //   host: process.env.WP_FTP_HOST,
     //   port: process.env.WP_FTP_PORT,
     //   user: process.env.WP_FTP_USER,
@@ -34,34 +28,41 @@ class Storage {
     // });
   }
   
+  async checkConnection() {
+    if (this.client.closed) {
+      await this.connectFTP();
+      return Promise.Resolve('Closed & Successfully Reconnected');
+    }
+    return Promise.resolve('Not Closed');
+  }
+  
   closeFTP() {
-    this.wp.close();
-    this.storage.close();
+    this.client.close();
   }
 
-  async uploadToWP(imagePath, path) {
+  async uploadSingle(imagePath, path) {
     let folders = path.split('/');
     folders.pop();
     folders = folders.join('/');
 
-    await this.wp.ensureDir(folders);
-    await this.wp.cd('/');
+    await this.client.ensureDir(folders);
+    await this.client.cd('/');
 
-    await this.wp.uploadFrom(imagePath, path);
+    await this.client.uploadFrom(imagePath, path);
 
     return Promise.resolve(true);
   }
 
-  async uploadToStorage(paths, destination) {
-    await this.storage.ensureDir(destination);
-    await this.storage.cd('/');
+  async uploadMultiple(paths, destination) {
+    await this.client.ensureDir(destination);
+    await this.client.cd('/');
 
     for (const path of paths) {
       try {
         const filename = path.split('/').pop();
         const filepath = destination + filename;
         
-        await this.storage.uploadFrom(path, filepath);
+        await this.client.uploadFrom(path, filepath);
       } catch (e) {
         console.log(e.message);
       }
