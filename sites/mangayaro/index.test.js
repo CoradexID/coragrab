ea53e9f8fd11453bdedda69ad8689437886916f8
coradex <coradex06@gmail.com>
2022-06-28 20:43:33 +0700

    Scripted auto-commit on change (2022-06-28 20:43:33) by gitwatch.sh

--- sites/mangayaro/index.test.js
+++ sites/mangayaro/index.test.js
@@ -6,53 +6,50 @@ const scraper = require(process.env.HOME_DIR + 'App/Scraper/' + process.env.MAIN
 
 async function run() {
   const db = new Database();
-  
+
   await db.connectDatabase();
   await db.connectStorage();
 
   try {
     const feeds = await scraper.getFeed();
-    for (var i = 0; i < feeds.length; i++) {
-      if (i == feeds.length - 1) {
-        const feed = feeds[i];
-        console.log(feed);
-        // MANGA CHECKER
-        const manga = await db.mangaCheck(feed);
-        console.log(manga);
+    for (const feed of feeds) {
+      console.log(feed);
+      // MANGA CHECKER
+      const manga = await db.mangaCheck(feed);
+      console.log(manga);
 
-        if (manga.status == 1) {
-          console.log('Getting Manga Data');
-          const mangaData = await scraper.getManga(feed.url);
-          mangaData.title = feed.title;
-          console.log('Posting Manga');
-          const insertedManga = await db.insertManga(mangaData);
-          console.log(insertedManga.post_title);
-          for (const chapter of mangaData.chapters) {
-            console.log('Getting Chapter Data');
-            const chapterData = await scraper.getChapter(chapter.url);
-            chapterData.chapter = chapter.chapter;
-            console.log('Posting Chapter');
-            const insertedChapter = await db.insertChapter(insertedManga.ID, chapterData);
-            console.log(insertedChapter.post_title);
-          }
+      if (manga.status == 1) {
+        console.log('Getting Manga Data');
+        const mangaData = await scraper.getManga(feed.url);
+        mangaData.title = feed.title;
+        console.log('Posting Manga');
+        const insertedManga = await db.insertManga(mangaData);
+        console.log(insertedManga.post_title);
+        for (const chapter of mangaData.chapters) {
+          console.log('Getting Chapter Data');
+          const chapterData = await scraper.getChapter(chapter.url);
+          chapterData.chapter = chapter.chapter;
+          console.log('Posting Chapter');
+          const insertedChapter = await db.insertChapter(insertedManga.ID, chapterData);
+          console.log(insertedChapter.post_title);
         }
+      }
 
-        if (manga.status == 0) {
-          const mangaData = await scraper.getManga(feed.url);
-          const chapters = await db.chapterCheck(manga.data.ID, mangaData);
-          for (const chapter of chapters) {
-            const chapterData = await scraper.getChapter(chapter.url);
-            chapterData.chapter = chapter.chapter;
-            const insertedChapter = await db.insertChapter(manga.data.ID, chapterData);
-            console.log(insertedChapter.post_title);
-          }
+      if (manga.status == 0) {
+        const mangaData = await scraper.getManga(feed.url);
+        const chapters = await db.chapterCheck(manga.data.ID, mangaData);
+        for (const chapter of chapters) {
+          const chapterData = await scraper.getChapter(chapter.url);
+          chapterData.chapter = chapter.chapter;
+          const insertedChapter = await db.insertChapter(manga.data.ID, chapterData);
+          console.log(insertedChapter.post_title);
         }
       }
     }
   } catch (e) {
     console.log(e.message);
   }
-  
+
   await db.closeDatabase();
   await db.closeStorage();
 
