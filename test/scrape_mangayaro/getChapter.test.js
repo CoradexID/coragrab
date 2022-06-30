1a5eb39fdb50de06b39a299c9951512058246203
coradex <coradex06@gmail.com>
2022-06-30 22:42:57 +0700

    Scripted auto-commit on change (2022-06-30 22:42:57) by gitwatch.sh

--- test/scrape_mangayaro/getChapter.test.js
+++ test/scrape_mangayaro/getChapter.test.js
@@ -4,7 +4,7 @@ const scraper = require('../../App/Scraper/Mangayaro.js');
   
   try {
   
-  const data = await scraper.getChapter('https://mangayaro.com/my-girlfriend-is-a-villain-chapter-112-bahasa-indonesia/', false, {replaceImageDomain: process.env.MIRROR_URL});
+  const data = await scraper.getChapter('https://mangayaro.com/my-girlfriend-is-a-villain-chapter-110-bahasa-indonesia/', false, {replaceImageDomain: process.env.MIRROR_URL});
   console.log(data);
   
   } catch (e) {
