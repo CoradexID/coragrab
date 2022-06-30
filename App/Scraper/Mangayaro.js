d6c61517966af0ff3df05ef36884239de6bf2c05
coradex <coradex06@gmail.com>
2022-06-30 16:07:12 +0700

    Scripted auto-commit on change (2022-06-30 16:07:12) by gitwatch.sh

--- App/Scraper/Mangayaro.js
+++ App/Scraper/Mangayaro.js
@@ -151,7 +151,7 @@ class Scraper {
 
       const results = [];
       upd.each(function(v, i) {
-        if (v == 2) {
+        if (v == 3) {
           const manga = $(this).find('.utao .imgu a.series');
           manga.each(function(v, i) {
             const title = $(this).attr('title');
