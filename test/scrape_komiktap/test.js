3d2acf605d054d13f3c832833e8a523213abce25
= <=>
2022-07-12 13:06:31 +0700

    Scripted auto-commit on change (2022-07-12 13:06:30) by gitwatch.sh

--- test/scrape_komiktap/test.js
+++ test/scrape_komiktap/test.js
@@ -4,7 +4,10 @@ const { JSDOM } = require('jsdom');
 (async () => {
   const res = await axios.get('https://manhwaid.fun/manga/e-romance/chapter-12/');
   const dom = new JSDOM(res.data).window.document;
-  const images = dom.querySelector('.reading-content').innerHTML;
+  const images = dom.querySelectorAll('.reading-content img');
   
-  console.log(images);
+  for (const image of images) {
+    console.log(image.src);
+  }
+  
 })();
