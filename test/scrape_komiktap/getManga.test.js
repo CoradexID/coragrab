51970231b668b83ba2c95707527a830ce0563aa0
= <=>
2022-07-12 12:35:43 +0700

    Scripted auto-commit on change (2022-07-12 12:35:43) by gitwatch.sh

--- test/scrape_komiktap/getManga.test.js
+++ test/scrape_komiktap/getManga.test.js
@@ -2,7 +2,7 @@ const scraper = require('../../App/Scraper/Komiktap.js');
 
 (async () => {
   
-  const data = await scraper.getManga('https://mangayaro.com/manga/the-raven-black-hero/');
+  const data = await scraper.getManga('https://194.233.66.232/manga/sextudy-group/');
   console.log(data);
   
 })();
