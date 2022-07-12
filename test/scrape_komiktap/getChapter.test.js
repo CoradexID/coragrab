b15fc592bc908c6aaab1b116304a6e5654d3426f
= <=>
2022-07-12 12:46:38 +0700

    Scripted auto-commit on change (2022-07-12 12:46:38) by gitwatch.sh

--- test/scrape_komiktap/getChapter.test.js
+++ test/scrape_komiktap/getChapter.test.js
@@ -1,10 +1,11 @@
-const scraper = require('../../App/Scraper/Mangayaro.js');
+const scraper = require('../../App/Scraper/Komiktap.js');
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 (async () => {
   
   try {
   
-  const data = await scraper.getChapter('https://mangayaro.com/my-girlfriend-is-a-villain-chapter-110-bahasa-indonesia/', false, {replaceImageDomain: process.env.MIRROR_URL});
+  const data = await scraper.getChapter('https://mangayaro.com/my-girlfriend-is-a-villain-chapter-110-bahasa-indonesia/');
   console.log(data);
   
   } catch (e) {
