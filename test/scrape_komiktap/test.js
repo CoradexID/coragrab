605be6a1f97b1de2e1989b916b077055f507de72
= <=>
2022-07-12 13:01:51 +0700

    Scripted auto-commit on change (2022-07-12 13:01:51) by gitwatch.sh

--- test/scrape_komiktap/test.js
+++ test/scrape_komiktap/test.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 
 (async () => {
-  const res = await axios.get(url);
+  const res = await axios.get('https://manhwaid.fun/manga/e-romance/chapter-12/');
   console.log(res.data);
 })();
